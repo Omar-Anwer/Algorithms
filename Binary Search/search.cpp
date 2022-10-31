10b217be8feec54be4b9e9f767119af4c8517019
Omar Anwer <68167265+Omar-Anwer@users.noreply.github.com>
2022-10-31 10:39:44 +0200

    Update search.cpp

--- Binary Search/search.cpp
+++ Binary Search/search.cpp
@@ -4,34 +4,59 @@ using namespace std;
 static int iterations;
 
 typedef int Type;
+#define KEY_NOT_FOUND -1
 
-size_t binarySearch(Type a[], size_t start, size_t end, Type val)
+size_t binarySearch(Type a[], size_t start, size_t end, Type key)
 {
     while (start <= end)
     {
         size_t mid = start + ((end - start) >> 1);   // x/2 == x>>1
         iterations++;
-        if (val == a[mid]){
+        if (key == a[mid]) {
             return mid;       // return index of element
         }
-        else if (val < a[mid]){
+        if (key < a[mid]) {
             end = mid - 1;
         }
         else {
             start = mid + 1;
         }
+        
     }
-    return -1;
+    return KEY_NOT_FOUND;
 }
+
+size_t binarySearchV2(Type a[], size_t start, size_t end, Type key)
+{
+    while (start < end)
+    {
+        size_t mid = start + ((end - start) >> 1);   // x/2 == x>>1
+        iterations++;
+        if (key >  a[mid]) {
+            start = mid + 1;
+        }
+        else {
+            end = mid;
+        }
+    }
+
+    if (start == end && key == a[start]) {
+        return start; // return index of element
+    }
+        
+    return KEY_NOT_FOUND;
+}
+
+
 
 int main()
 {
     int arr[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-    int idx = binarySearch(arr, 0, 9, 99);
+    int idx = binarySearchV2(arr, 0, 9, 4);
 
 
-    if (idx == -1){
+    if (idx == KEY_NOT_FOUND){
         cout << "NO" << "\n";
     }
     else{
